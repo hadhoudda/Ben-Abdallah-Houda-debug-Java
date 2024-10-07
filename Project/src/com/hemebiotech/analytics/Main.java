b91391701884edb0725ca6faa33bcdb4feb2f91b
Houda Ben Abdallah <ahhouda@yahoo.fr>
2024-10-07 09:43:37 +0200

    nettoyage de code

--- Project/src/com/hemebiotech/analytics/Main.java
+++ Project/src/com/hemebiotech/analytics/Main.java
@@ -8,8 +8,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ISymptomReader reader = new ReadSymptomDataFromFile("Project02Eclipse/symptoms.txt");
-        ISymptomWriter writer = new WriteSymptomDataToFile("result.out");
+        ISymptomReader reader = new ReadSymptomDataFromFile("Project/Resources/symptoms.txt");
+        ISymptomWriter writer = new WriteSymptomDataToFile("Project/Resources/result.out");
         AnalyticsCounter analyticsCounter = new AnalyticsCounter(reader, writer);
         //read symptoms
         List<String> listSymtoms  = analyticsCounter.getSymptoms();
